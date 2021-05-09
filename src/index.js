52d39a35effdf6108734f1fedb7f05f8e282e231
Ankichi <59509443+An39nA0219@users.noreply.github.com>
2021-05-10 02:27:54 +0900

    todo追加機能（ボタン）

--- src/index.js
+++ src/index.js
@@ -17,8 +17,26 @@ const onClickAdd = () => {
   p.className = "list-name";
   p.innerText = inputText;
 
+  // 完了ボタン生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("complete");
+  });
+
+  // 削除ボタン生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグを削除
+    const deleteTarget = deleteButton.closest("li");
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
   // 入れ子構造の作成
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   li.appendChild(div);
 
   // 未完了ulに追加
